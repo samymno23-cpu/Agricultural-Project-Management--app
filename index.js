a68e01a0bfbbf0ceb09e7d414f0f6d28b0e91b97
سامي سعد <samymno23@gmail.com>
2026-02-01 00:20:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const app = express();
 const path = require('path');
 
 app.use(express.json());
-app.use(express.static('public')); // تأكد أن اسم المجلد هو public
+app.use(express.static('public'));
 
-// رابط المزرعة الذكي
 app.get('/farm/:farmId', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`السيرفر يعمل على: http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
